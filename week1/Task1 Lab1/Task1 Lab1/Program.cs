91f27debc328aa025e8e138b95408dc37c21f73e
Adilbek <adilbek.zhenisbek1@gmail.com>
2019-01-27 16:13:25 +0600

    Task1

---
+++ week1/Task1 Lab1/Task1 Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>{0};
+            int n = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            int[n] nums = new int[n];
+            for (int i = 2; (i * i) <= n; i++)
+            {
+                if( n % i == 0)
+                {
+                    cnt++;
+                    numbers.Add(i);
+                }
+                Console.WriteLine(cnt);
+                Console.WriteLine(numbers);
+                Console.ReadLine();
+            }
+        }
+    }
+}
