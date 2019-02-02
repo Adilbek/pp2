2ed62ff0531312b23a8f3af6349be6c9678535f4
Adilbek <adilbek.zhenisbek1@gmail.com>
2019-02-02 11:11:54 +0600

    Task 4 with comment

---
+++ week1/Task4/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int razmer = int.Parse(Console.ReadLine()); // sozdaem peremennuyu dlya razmera
+            for (int i = 1; i <= razmer; i++)// column nachinaetsya s edinichki do razmera
+            {
+                for (int j = 1; j <= i; j++)// row budet uvelichivat'sya s edinicki do column
+                    Console.Write("[*]"); // pomechaem [*]
+                Console.WriteLine(" ");// piwem kazhdyi sludyuwiy cikl v druguyu liniyu
+                    
+                    
+            }
+           Console.ReadKey();
+        }
+    }
+}
