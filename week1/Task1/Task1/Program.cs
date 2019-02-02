a74b904359a2f8ede2ddfdabd42406ce260b5a9a
Adilbek <adilbek.zhenisbek1@gmail.com>
2019-02-02 10:45:18 +0600

    dobavil commentary

--- week1/Task1/Task1/Program.cs
+++ week1/Task1/Task1/Program.cs
@@ -5,36 +5,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Week2
+namespace Task1
 {
     class Program
     {
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            int[] arr = new int[n];
-            string[] s = Console.ReadLine().Split(); // 1 2 3 4 5
+            int[] arr = new int[n]; // sozdaem massiv
+            string[] s = Console.ReadLine().Split(); 
             for (int i = 0; i < n; i++)
             {
                 arr[i] = int.Parse(s[i]);
             }
 
-            f1(arr, n);
+            f1(arr, n); // vyzyvaem methode pod nazvaniem f1
 
         }
 
-        public static void f1(int[] a, int m)
+        public static void f1(int[] a, int m) // peredaem massiv i peremennuyu m (razmernost')
         {
-            List<int> l = new List<int>(2 * m);
+            List<int> l = new List<int>(2 * m); // sozdaem list razmerom 2*array
             for (int i = 0; i < m; i++)
             {
-                l.Add(a[i]);
+                l.Add(a[i]);// dva raza dobavlyayu v list
                 l.Add(a[i]);
             }
 
             foreach (int s3 in l)
             {
-                Console.Write(s3 + " ");
+                Console.Write(s3 + " ");// vyvod s probelom s odnu stroku
 
             }
             Console.ReadKey();
