3e93ac712e41c74965f63c3f3dab08480b6b5055
Adilbek <adilbek.zhenisbek1@gmail.com>
2019-02-09 09:48:13 +0600

    Week2

---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            for(int i = 0; i < s.Length/2; i++)
+            {
+                if(s[i] != s[s.Length - i - 1])
+                {
+                    Console.WriteLine("NO");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                  
+                } 
+
+            }
+            
+            Console.WriteLine("YES");
+            Console.ReadKey();
+
+        }
+    }
+}
