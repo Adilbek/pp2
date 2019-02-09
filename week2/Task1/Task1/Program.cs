281bd8e18dc13afcf9dcae06a9eee6f31efb5f06
Adilbek <adilbek.zhenisbek1@gmail.com>
2019-02-09 09:58:11 +0600

    Week2

---
+++ week2/Task1/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine(); // sozdaem stroku i vvodim s klaviatury
+
+            for (int i = 0; i < s.Length / 2; i++) // ciklom probegaemsya do poloviny razmera stroki
+            {
+                if (s[i] != s[s.Length - i - 1]) // esli [i] ne sozpadaet s length - i - 1 cout No
+                {
+                    Console.WriteLine("NO"); // delim stroku na dve chasti i proveryam sovpadaet li pervyi element s poslednim; vtoroi s predposl, i tak dalee
+                                  
+                    Console.ReadKey();
+                    Environment.Exit(0);
+
+                }
+
+            }
+
+            Console.WriteLine("YES");
+            Console.ReadKey();
+
+        }
+    }
+}
--- week2/Task1/Task1/Properties/AssemblyInfo.cs
+++ week2/Task1/Task1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task2")]
+[assembly: AssemblyTitle("Task1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task2")]
+[assembly: AssemblyProduct("Task1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("32cb5129-e3e2-4e9e-9887-729b8e73dc4e")]
+[assembly: Guid("e01ca540-867e-42aa-8cb5-db839a403a52")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
